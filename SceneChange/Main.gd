5806337d6d01b2a34a91b86d1880ee57d2112530
Mario Resa <80423737+Mario999X@users.noreply.github.com>
2023-03-16 14:20:09 +0100

    Cambio de escenas.

---
+++ SceneChange/Main.gd
@@ -0,0 +1,9 @@
+extends Node
+
+
+func _on_Escena1_btn_pressed():
+	get_tree().change_scene("res://menus/Escena1.tscn")
+
+
+func _on_Escena2_btn_pressed():
+	get_tree().change_scene("res://menus/Escena2.tscn")
---
+++ SceneChange/menus/Escena2.gd
@@ -0,0 +1,4 @@
+extends Node
+
+func _on_ButtonBack_pressed():
+	get_tree().change_scene("res://Main.tscn")
